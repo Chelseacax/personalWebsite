934c735cf27149ff8b5597a3c374b11534ab4e03
Chelseacax <chelseazhao233@gmail.com>
2023-09-04 18:58:26 +0800

    add the first edition

---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Delivery Turtlebot',
+    description: `A robotics project that involves designing and building a delivery robot that can autonomously navigate through indoor environments and deliver can drinks to designated locations. The project can utilise the Turtlebot platform, which uses ROS2 (Robot Operating System 2) and is equipped with sensors,lidar, and other hardware components. The final product can be used for practical applications such as delivering items in a workplace or home setting. The project involves programming in Python or C++.`,
+    imgSrc: '/static/images/turtlebot3-burger.jpg',
+    href: 'https://docs.google.com/document/d/12iMz_IQ1G9Wm2oOXZOY0jznhilAQFaWyv4wf_CHxV0o/edit',
+  },
+  {
+    title: 'Tiktok API Gateway for microservices',
+    description: `Group project under ByteDance and NUS Orbital. Implemented service handlers, routers, and load balancing for efficient communication with microservices. Utilised etcd for dynamic service discovery and leveraged JSON mapping for generic calls. Conducted comprehensive testing and evaluation, ensuring high performance and scalability. Golang, Hertz, Kitex, Etcd.`,
+    imgSrc: '/static/images/image.png',
+    href: 'https://docs.google.com/document/d/1_plI0Uo0BMp_dmGk1guOZPVVn63LVT6k3Y7NAu9aBss/edit#heading=h.adggkmlzkayu',
+  },
+]
+
+export default projectsData
